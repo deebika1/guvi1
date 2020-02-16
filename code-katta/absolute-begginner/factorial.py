b446459e78be79887df92b72e43b7abe075874e7
Durgadevi23 <60968388+Durgadevi23@users.noreply.github.com>
2020-02-16 12:12:02 +0530

    Update factorial.py

--- code-katta/absolute-begginner/factorial.py
+++ code-katta/absolute-begginner/factorial.py
@@ -1,1 +1,17 @@
+#Program to find factorial of a given number
+#Get input from user
+N=int(input(""))
+#Initialize the value
+fact=1
+#Check the number is positive
+def facto():
+    fact=1
+    if(N>0):
+        for i in range(1,N+1):
+            fact=fact*i
+        return fact
+    elif(N==0):
+        return 0
 
+facto()
+print(facto())
