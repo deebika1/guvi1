7b14a30f48a9749ee8ff802845750c65e6921d57
Durgadevi23 <60968388+Durgadevi23@users.noreply.github.com>
2020-02-17 08:21:34 +0530

    Create form_triangle.py

---
+++ code-katta/Basics/form_triangle.py
@@ -0,0 +1,13 @@
+#check the input whether form a triangle or not
+def triangle():  
+    A, B,C = map(int, input().split())
+    y="yes"
+    n="no"
+    # check condition  
+    if (A + B <= C) or (A + C <= B) or (B + C <= A) : 
+        print(n)
+    else: 
+        print(y)
+    return y,n
+res=triangle()
+print()
