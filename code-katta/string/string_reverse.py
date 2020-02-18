ae5f21911524b14a0d8462f863ccff343018613a
Durgadevi23 <60968388+Durgadevi23@users.noreply.github.com>
2020-02-18 15:19:21 +0530

    Create string_reverse.py

---
+++ code-katta/string/string_reverse.py
@@ -0,0 +1,15 @@
+#Reserve the string
+#Define the function
+def string_reverse(a):
+    rstr1 = ''
+    index = len(a)
+    while index > 0:
+        rstr1 += a[ index - 1 ]
+        index = index - 1
+        
+    return rstr1
+#Get input from user
+a=input()
+#Calling the function
+b=string_reverse(a)
+print(string_reverse(a).capitalize())
