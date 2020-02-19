474604cae3ee008028c1268a28596bb77e2b74f0
Durgadevi23 <60968388+Durgadevi23@users.noreply.github.com>
2020-02-19 13:09:11 +0530

    Create stone_paper.py

---
+++ code-katta/Basics/stone_paper.py
@@ -0,0 +1,11 @@
+#Program for stone paper scisscor game
+#Get two input
+A1,A2=input().split()
+#check gaming condition
+if(A1=="R" and A2=="P" or (A1=="S" and A2=="R")or (A1=="P" and A2=="S")):
+    print(A2)
+elif(A1=="R" and A2=="S" or(A1=="P" and A2=="R")or(A1=="S" and A2=="P") ):
+    print(A1)
+elif(A1==A2):
+    print("D")
+
