a5c667408fa5fc8ee7377b298deb4849cfc8467b
Durgadevi23 <60968388+Durgadevi23@users.noreply.github.com>
2020-02-20 10:37:27 +0530

    Create N_followed_by_K.py

---
+++ code-katta/Basics/N_followed_by_K.py
@@ -0,0 +1,11 @@
+#program to find k exist in the list
+N,K=map(int,input().split())
+li=list[map(int,input().split())]
+count=0
+for i in range(len(n)):
+	if(n[i]==k):
+    	count=count+1
+if(count!=0):
+	print("yes")
+else:
+	print("no")
