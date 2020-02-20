4745c9fbafa37f44dd0dbe2bcff45e01cf479b85
Durgadevi23 <60968388+Durgadevi23@users.noreply.github.com>
2020-02-20 22:18:55 +0530

    Create holiday_not.py

---
+++ code-katta/string/holiday_not.py
@@ -0,0 +1,7 @@
+#Program to find holiday or not
+S=input()
+#check input is holiday or not
+if(S=="monday" or S=="tuesday" or S=="wednesday" or S=="thursday" or S=="friday"):
+	print("no")
+elif(S=="saturday" or S=="sunday"):
+	print("yes")
