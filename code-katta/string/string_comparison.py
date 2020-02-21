2c50d834cae7e2731d697ffc14c300c56f71fe40
Durgadevi23 <60968388+Durgadevi23@users.noreply.github.com>
2020-02-21 09:42:53 +0530

    Create string_comparison.py

---
+++ code-katta/string/string_comparison.py
@@ -0,0 +1,8 @@
+#Program to find two string or equal or not
+#Get input from user
+S1,S2=map(str,input().split())
+#check two input are equal or not
+if(S1==S2):
+  print("yes")
+else:
+  print("no")
