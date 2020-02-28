bb6e11eaf652c385a9fc733a3b94c894acad3cca
Durgadevi23 <60968388+Durgadevi23@users.noreply.github.com>
2020-02-28 07:33:18 +0530

    Create area_circle.js

---
+++ javascript/area_circle.js
@@ -0,0 +1,8 @@
+#area of circle
+const readline = require('readline');
+const inp = readline.createInterface({input:process.stdin});
+inp.on("line",(data)=>{
+  
+  cir=2*3.14159*data;
+ console.log(cir.toFixed(2));
+});
