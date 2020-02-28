21a910cd8bf31ca15582febabdbecc3c1c4f7a32
Durgadevi23 <60968388+Durgadevi23@users.noreply.github.com>
2020-02-28 11:16:01 +0530

    Update no_of_days_month.js

--- javascript/no_of_days_month.js
+++ javascript/no_of_days_month.js
@@ -1,20 +1,13 @@
 #number of days in month
-const readline = require('readline');
-const inp = readline.createInterface({input:process.stdin});
+const readline=require('readline');
+const inp=readline.createInterface({input:process.stdin});
 inp.on("line",(data)=>{
-  if(data==1 || data==3 || data==5 || data==7 || data==8 || data==10 || data==12)
-  {
-    console.log("31")
-  }
-  else if(data==4 || data==6 || data==9 || data==11)
-  {
-    console.log("30")
-  }
-  else if(data==2)
-  {
+  if(data==2)
     console.log("28")
-  }
-  else{
-    console.log("error")
-  }
+  else if(data==1 || data==3 || data==5||data==7||data==8||data==10||data==12)
+    console.log("31")
+  else if(data==4 || data==6|| data==9||data==11)
+    console.log("30")
+  else
+    console.log("Error")
 });
