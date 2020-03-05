65251ff68242c155322d9be910ef9bc05d0b1b2c
Durgadevi23 <60968388+Durgadevi23@users.noreply.github.com>
2020-03-05 11:07:44 +0530

    Create password_verification.py

---
+++ assessment.py/password_verification.py
@@ -0,0 +1,13 @@
+#password verification
+import hashlib
+user="Durga"
+password="Durga23"
+word=hashlib.sha256(password.encode()).hexdigest()
+for i in range(1,6):
+    user1=input("Enter username")
+    password1=input("Enter password: ")
+    pass1=hashlib.sha256(password1.encode()).hexdigest()
+    if(user==user1):
+        if(pass1==word):
+            print("ok")
+            break
