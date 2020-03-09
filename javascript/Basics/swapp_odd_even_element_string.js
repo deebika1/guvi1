7829145fdd76931e23613081ec0e9511ab31eaac
Durgadevi23 <60968388+Durgadevi23@users.noreply.github.com>
2020-03-09 21:55:19 +0530

    Create swapp_odd_even_element_string.js

---
+++ javascript/Basics/swapp_odd_even_element_string.js
@@ -0,0 +1,21 @@
+const readline = require('readline');
+const inp = readline.createInterface({input: process.stdin});
+//const n=[];
+inp.on("line", (data) => {
+  var a=data.split("")
+  //console.log(a)
+    var l= '';
+for(i=0;i<a.length-1;i++)
+{
+   var s=a[i+1]
+   var b=a[i]
+    l+=s
+    l+=b
+    i=i+1
+}
+if((a.length%2)!=0)
+{
+    l+=a[a.length-1]
+}
+console.log(l)
+});
